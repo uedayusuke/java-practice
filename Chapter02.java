05533f26293b4093e470878ebfe8500e364117a6
uedayusuke <uedayusuke1107@gmail.com>
2020-08-14 11:23:31 +0900

    START

--- Chapter02.java
+++ Chapter02.java
@@ -1,7 +1,7 @@
 
 public class Chapter02 {
 	public static void main(String[] args) {
-		System.out.println("Hello WEBCAMP!");
-		System.out.println("Hello Java!");
+		System.out.println("Hello WEBCAMP");
+		System.out.println("Hello Java");
 	}
 }
