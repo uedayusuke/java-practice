3760fba39ddc1e6f7d653f83c511a374e7e6609b
uedayusuke <uedayusuke1107@gmail.com>
2020-08-14 12:57:30 +0900

    [FINISH]4章確認問題

---
+++ Chapter04.java
@@ -0,0 +1,15 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str1;
+		str1 = "yusuke";
+		System.out.println(str1);
+
+		str1 = "push";
+		System.out.println(str1);
+
+		final int LUCKY_NUMBER = 3;
+		System.out.println(LUCKY_NUMBER);
+
+	}
+}
