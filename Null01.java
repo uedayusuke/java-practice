31f2e27a409ae7f4240aa001ffe8b00ec7be54c0
uedayusuke <uedayusuke1107@gmail.com>
2020-08-15 16:33:55 +0900

    [ADD]5章配列

--- Null01.java
+++ Null01.java
@@ -15,5 +15,17 @@ public class Null01 {
 		Integer integer2 = Integer.valueOf(5);
 		int int1 = integer1;
 		int int2 = integer2.intValue();
+
+		int[] array1 = new int[3];
+		array1[0] = 60;
+		array1[2] = 20;
+		array1[1] = 20;
+		int int3 = array1[2];
+
+		int array2[] = null;
+
+		int[] array3 = new int[] { 10, 20, 50 };
+		int[] array4 = { 10, 20, 50};
+
 	}
 }
