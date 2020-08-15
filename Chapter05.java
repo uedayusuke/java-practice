8906fe2fc25284c2b6828793ebbb24a9bce38ee7
uedayusuke <uedayusuke1107@gmail.com>
2020-08-15 22:34:58 +0900

    [ADD]プリミティブ型

--- Chapter05.java
+++ Chapter05.java
@@ -1,6 +1,21 @@
 
 public class Chapter05 {
 	public static void main(String[] args) {
+		// プリミティブ型
+		// long型に変数を代入して出力
+		long long1;
+		long1 = 100000L;
+		System.out.println(long1);
+
+		// double型に変数を代入して出力
+		double double1;
+		double1 = 0.14;
+		System.out.println(double1);
+
+		// boolean型に変数を代入して出力
+		boolean boolean1;
+		boolean1 = false;
+		System.out.println(boolean1);
 
 	}
 }
