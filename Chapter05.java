b5b7ff7e817c84f6ad564ea05ded950959979564
uedayusuke <uedayusuke1107@gmail.com>
2020-08-15 22:45:44 +0900

    [ADD]ラッパークラス

--- Chapter05.java
+++ Chapter05.java
@@ -2,20 +2,37 @@
 public class Chapter05 {
 	public static void main(String[] args) {
 		// プリミティブ型
-		// long型に変数を代入して出力
+		// long型に値を代入して出力
 		long long1;
 		long1 = 100000L;
 		System.out.println(long1);
 
-		// double型に変数を代入して出力
+		// double型に値を代入して出力
 		double double1;
 		double1 = 0.14;
 		System.out.println(double1);
 
-		// boolean型に変数を代入して出力
+		// boolean型に値を代入して出力
 		boolean boolean1;
 		boolean1 = false;
 		System.out.println(boolean1);
+
+		// ラッパークラス
+		// Floatに値を代入して出力
+		Float float1;
+		float1 = 3.14f;
+		System.out.println(float1);
+
+		// Integerに値を代入して出力
+		Integer int1;
+		int1 = 1122;
+		System.out.println(int1);
+
+		// Characterに値を代入して出力
+		Character char1;
+		char1 = 'う';
+		System.out.println(char1);
+
 
 	}
 }
