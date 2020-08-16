e121fd5edb509a16c817af643e673df25f87b998
uedayusuke <uedayusuke1107@gmail.com>
2020-08-16 11:28:52 +0900

    [ADD]6章算術演算子

--- training06.java
+++ training06.java
@@ -2,5 +2,26 @@
 public class training06 {
 	public static void main(String[] args) {
 		// 6章【演算子を学ぼう】
+		// 算術演算子
+		int a;
+		a = 15;
+		int c ;
+		// 加算
+		c = a + 3;
+		System.out.println(c);
+		// 減算
+		c = a - 5;
+		System.out.println(c);
+		// 乗算
+		c = a * 2;
+		System.out.println(c);
+		// 除算
+		c = a / 6;
+		System.out.println(c);
+		// 剰余算
+		c = a % 4;
+		System.out.println(c);
+
+
 	}
 }
