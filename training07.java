5fe6938564cb13cb73a02493d4348b68f0eda4bb
uedayusuke <uedayusuke1107@gmail.com>
2020-08-17 12:41:00 +0900

    [ADD]7章if文

--- training07.java
+++ training07.java
@@ -1,6 +1,17 @@
 
 public class training07 {
 	public static void main(String[] args) {
-
+		// 7章【条件分岐を学ぼう】
+		// if文
+		int number = 7;
+		if (number < 5) {
+			System.out.println(number + "は5未満です");
+		}
+		if (number < 10) {
+			System.out.println(number + "は10未満です");
+		}
+		if (number < 15) {
+			System.out.println(number + "は15未満です");
+		}
 	}
 }
