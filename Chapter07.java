6f54ffc5e7788898f0ca87be2ffc9e145276e08d
uedayusuke <uedayusuke1107@gmail.com>
2020-08-17 13:07:52 +0900

    [ADD]問題1

--- Chapter07.java
+++ Chapter07.java
@@ -1,6 +1,17 @@
 
 public class Chapter07 {
 	public static void main(String[] args) {
+		// 問題1
+		int dt = 90;
+		if (dt <= 5) {
+			System.out.println("とても近いです");
+		} else if (dt <= 10) {
+			System.out.println("まぁ近いです");
+		} else if (dt <= 15) {
+			System.out.println("まぁ遠いです");
+		} else {
+			System.out.println("とても遠いです");
+		}
 
 	}
 }
