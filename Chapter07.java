dde03995e0a9b10ce77ddb0000ecd717c6812e90
uedayusuke <uedayusuke1107@gmail.com>
2020-08-17 13:17:44 +0900

    [FINISH]7章確認問題

--- Chapter07.java
+++ Chapter07.java
@@ -13,5 +13,21 @@ public class Chapter07 {
 			System.out.println("とても遠いです");
 		}
 
+		// 問題2
+		String signal = "blue";
+		switch (signal) {
+			case "red":
+				System.out.println("赤信号です");
+				break;
+			case "yellow":
+				System.out.println("黄信号です");
+				break;
+			case "blue":
+				System.out.println("青信号です");
+				break;
+			default:
+				System.out.println("信号の色ではありません");
+		}
+
 	}
 }
