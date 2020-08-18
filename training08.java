6a6e6ffa1e04cca3be873b4dbd47b41966dcb78d
uedayusuke <uedayusuke1107@gmail.com>
2020-08-18 20:12:43 +0900

    [ADD]8章for文

--- training08.java
+++ training08.java
@@ -3,17 +3,23 @@ public class training08 {
 	public static void main(String[] args) {
 		// 8章【繰り返しを学ぼう】
 		// while文
-		int number = 1;
+		int number = 100;
 		while (number < 50) {
 			number *= 2;
-			System.out.println("training08 = " + number);
+			System.out.println("while = " + number);
 		}
 
 		//do-while文
 		number = 1;
 		do {
 			number *= 2;
-			System.out.println("training08 = " + number);
+			System.out.println("do-while = " + number);
 		}while (number < 50);
+		// while文とdo-while文の違いは一番最初の処理をやるかやらないか
+
+		// for文
+		for (int num = 1; num <= 5; num++) {
+			System.out.println("for = " + num);
+		}
 	}
 }
