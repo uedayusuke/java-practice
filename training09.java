0f1c6ebe019f59ce79408745c4016ad37b1209d8
uedayusuke <uedayusuke1107@gmail.com>
2020-08-19 16:56:13 +0900

    [ADD]クラスを出力

--- training09.java
+++ training09.java
@@ -2,5 +2,11 @@
 public class training09 {
 	public static void main(String[] args) {
 		// 9章【クラスとメソッドを理解しよう】
+		// クラス
+		Human01 yamada = new Human01();
+		System.out.println("名前は" + yamada.name + "で、年齢は" + yamada.age + "です。");
+
+		Human01 sato = new Human01("佐藤", 25);
+		System.out.println("名前は" + sato.name + "で、年齢は" + sato.age + "です。");
 	}
 }
