314713d03b594cb1730ad8772f26e2ab2a56053f
uedayusuke <uedayusuke1107@gmail.com>
2020-08-19 16:49:56 +0900

    [ADD]クラスの追加

--- Human01.java
+++ Human01.java
@@ -1,4 +1,16 @@
 
 public class Human01 {
 	// 9章【クラスとメソッドを理解しよう】
+	public String name;
+	public int age;
+
+	public Human01() {
+		name = "山田";
+		age = 20;
+	}
+
+	public Human01(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
 }
