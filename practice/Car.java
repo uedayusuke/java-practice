850b8bc12c594dea2ca1f79e18804a29d9b5d70e
uedayusuke <uedayusuke1107@gmail.com>
2020-08-20 12:50:31 +0900

    [FINISH]9章確認問題

--- practice/Car.java
+++ practice/Car.java
@@ -5,4 +5,18 @@ public class Car {
 	private double fuelCost;
 	// 残量(L)
 	private double fuelAmount;
+
+	public Car(double fuelCost, double fuelAmount) {
+		this.fuelCost = fuelCost;
+		this.fuelAmount = fuelAmount;
+	}
+
+	public void move(int km) {
+		System.out.println(km + "km走ります");
+		this.fuelAmount -= (km / fuelCost);
+	}
+
+	public double getFuelAmount() {
+		return this.fuelAmount;
+	}
 }
--- practice/Chapter09.java
+++ practice/Chapter09.java
@@ -10,7 +10,6 @@ public class Chapter09 {
 		Car car = new Car(fuelCost, fuelAmount);
 
 		car.move(20);
-		System.out.println("残量は、" + car.getFuelAmount() + "です。");
-
+		System.out.println("残量は、" + car.getFuelAmount() + "です");
 	}
 }
