85505ab65e22cdfa1c72c9103e7edad1b641c963
uedayusuke <uedayusuke1107@gmail.com>
2020-08-20 11:58:37 +0900

    [ADD]9章メソッド

--- training/human/Human02.java
+++ training/human/Human02.java
@@ -3,5 +3,26 @@ package training.human;
 public class Human02 {
 	// 9章【クラスとメソッドを理解しよう】
 	// メソッド
+	public String name;
+	public int age;
+	public String profession;
+
+	public Human02() {
+		this.name = "田中";
+		this.age = 20;
+		this.profession = "プログラマー";
+	}
+
+	public String getProfile() {
+		return "年齢は" + this.age + "、職業は" + this.profession;
+	}
+
+	public void greet(String friend) {
+		if (friend == null) {
+			System.out.println("挨拶する友達がわかりません！");
+			return;
+		}
+		System.out.println(friend + "さん、こんにちは!”");
+	}
 
 }
--- training/training09.java
+++ training/training09.java
@@ -2,6 +2,7 @@
 package training;
 // training.human.Human01をインポート
 import training.human.Human01;
+import training.human.Human02;
 
 public class training09 {
 	public static void main(String[] args) {
@@ -12,6 +13,18 @@ public class training09 {
 
 		Human01 sato = new Human01("佐藤", 25);
 		System.out.println("名前は" + sato.name + "で、年齢は" + sato.age + "です。");
+
+		// メソッド
+		Human02 tanaka = new Human02();
+
+		System.out.println("私の名前は、" + tanaka.name + "です。");
+		String profile = tanaka.getProfile();
+		System.out.println(profile + "です。");
+
+		tanaka.greet("田邊");
+		tanaka.greet(null);
+
+
 
 
 	}
