df165fffd06bebe4c8e49c273b1539b7e851e1ed
uedayusuke <uedayusuke1107@gmail.com>
2020-08-20 12:23:04 +0900

    [ADD]9章修飾子

--- training/human/Human03.java
+++ training/human/Human03.java
@@ -4,4 +4,24 @@ public class Human03 {
 	// 9章【クラスとメソッドを理解しよう】
 	// 修飾子
 
+	// static変数
+	static public int humanCount = 0;
+	public String name;
+	// 定数フィールド
+	public static final String GREETING = "こんにちは";
+
+	public Human03(String name) {
+		this.name = name;
+		Human03.humanCount++;
+	}
+
+	static public void staticMethodPrint() {
+		System.out.println("人の数は" + Human03.humanCount);
+	}
+
+	public void instanceMethodPrint() {
+		System.out.println("名前は" + name);
+		System.out.println("人の数は" + Human03.humanCount);
+	}
+
 }
--- training/training09.java
+++ training/training09.java
@@ -1,8 +1,9 @@
 // パッケージがtrainingと宣言
 package training;
-// training.human.Human01をインポート
+// training.human.Human01~03をインポート
 import training.human.Human01;
 import training.human.Human02;
+import training.human.Human03;
 
 public class training09 {
 	public static void main(String[] args) {
@@ -24,8 +25,20 @@ public class training09 {
 		tanaka.greet("田邊");
 		tanaka.greet(null);
 
+		// 修飾子
+		Human03 satou = new Human03("佐藤");
+		Human03 kagami = new Human03("鏡");
 
+		Human03.staticMethodPrint();
+		System.out.println();
 
+		System.out.println(Human03.GREETING);
+
+		satou.instanceMethodPrint();
+		System.out.println();
+
+		kagami.instanceMethodPrint();
+		System.out.println();
 
 	}
 }
