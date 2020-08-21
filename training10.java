ece1fdc5512280ede431fc015c5f184518d3c55a
uedayusuke <uedayusuke1107@gmail.com>
2020-08-21 21:53:58 +0900

    [ADD]10章try-catch文

--- training10.java
+++ training10.java
@@ -4,8 +4,20 @@ public class training10 {
 		// 10章【例外の基本を学ぼう】
 		// 例外の基本
 		System.out.println("100 ÷ 0 は?" );
-		int result = 100 / 0;
-		System.out.println("計算結果" + result);
-		System.out.println("プログラム終了");
+		//int result = 100 / 0;
+		//System.out.println("計算結果" + result);
+		//System.out.println("プログラム終了");
+
+		// 例外処理
+		// try-catch文（try-catch-finally文）
+		try {
+			System.out.println("100 ÷ 0 は?");
+			int result2 = 100 /0;
+			System.out.println("計算結果" + result2);
+		} catch (ArithmeticException e) {
+			System.out.println("例外が発生");
+		} finally {
+			System.out.println("プログラム終了");
+		}
 	}
 }
