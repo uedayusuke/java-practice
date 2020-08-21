0670a146ccd8199719d416ae7b7c76f2335b146f
uedayusuke <uedayusuke1107@gmail.com>
2020-08-21 21:43:24 +0900

    [ADD]10章例外の基本

--- training10.java
+++ training10.java
@@ -2,5 +2,10 @@
 public class training10 {
 	public static void main(String[] args) {
 		// 10章【例外の基本を学ぼう】
+		// 例外の基本
+		System.out.println("100 ÷ 0 は?" );
+		int result = 100 / 0;
+		System.out.println("計算結果" + result);
+		System.out.println("プログラム終了");
 	}
 }
