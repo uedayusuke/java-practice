61c92baab7152135bd084b504c9e7df9682e166f
uedayusuke <uedayusuke1107@gmail.com>
2020-08-22 17:32:10 +0900

    [ADD]try-catch文

--- practice/Chapter10.java
+++ practice/Chapter10.java
@@ -3,5 +3,16 @@ package practice;
 public class Chapter10 {
 	public static void main(String[] args) {
 		// 10章 確認問題
+		try {
+            int array[] = { 1, 3, 5 };
+            Chapter10.validIndex(array, 2);
+            Chapter10.validIndex(array, 3);
+
+            System.out.println("validIndexの呼び出し終了");
+        } catch (IllegalArgumentException e) {
+            System.out.println("IllegalArgumentExceptionが発生しました");
+            e.printStackTrace();
+        }
+
 	}
 }
