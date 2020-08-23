fd8eeb4124d108b6295843acd4694881eafefb16
uedayusuke <uedayusuke1107@gmail.com>
2020-08-23 17:08:22 +0900

    [ADD]9章コンストラクタ

--- human01.java
+++ human01.java
@@ -1,4 +1,18 @@
 
 public class human01 {
 	// 9章【クラスとメソッドを理解しよう】 復習
+	// 宣言
+	public String name;
+	public int age;
+
+	// 初期値（デフォルト）
+	public human01() {
+		name = "山田";
+		age = 25;
+	}
+	// 引数あり
+	public human01(String name, int age) {
+		this.name = name;
+		this.age =age;
+	}
 }
