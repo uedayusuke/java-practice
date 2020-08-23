f6e310c376c8b4ea239cb5154a80e39bbe072162
uedayusuke <uedayusuke1107@gmail.com>
2020-08-23 17:15:20 +0900

    [ADD]9章インスタンスの追加

--- reviwe09.java
+++ reviwe09.java
@@ -2,4 +2,10 @@
 public class reviwe09 {
 	// 9章【クラスとメソッドを理解しよう】 復習
 	// クラス
+	public static void main(String[] args) {
+		// 初期値のインスタンスを生成
+		human01 yamada = new human01();
+		// 初期値のフィールドを出力
+		System.out.println("名前は" + yamada.name + "で、年齢は" + yamada.age + "です");
+	}
 }
