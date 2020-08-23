de0b475b7f38019dcb354837e81e789b43597676
uedayusuke <uedayusuke1107@gmail.com>
2020-08-23 17:19:27 +0900

    [ADD]9章インスタンスの追加

--- reviwe09.java
+++ reviwe09.java
@@ -7,5 +7,10 @@ public class reviwe09 {
 		human01 yamada = new human01();
 		// 初期値のフィールドを出力
 		System.out.println("名前は" + yamada.name + "で、年齢は" + yamada.age + "です");
+
+		// 引数ありのインスタンスを生成
+		human01 zone = new human01("ゾーン", 39);
+		// 引数が代入されたフィールドを出力
+		System.out.println("名前は" + zone.name + "で、年齢は" + zone.age + "です");
 	}
 }
