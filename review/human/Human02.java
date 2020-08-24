cd62c71a10bff71cd6ec451d649749984026cf22
uedayusuke <uedayusuke1107@gmail.com>
2020-08-24 21:02:09 +0900

    [ADD]9章コンストラクタの追加

--- review/human/Human02.java
+++ review/human/Human02.java
@@ -2,4 +2,19 @@ package review.human;
 
 public class Human02 {
 	// 9章【クラスとメソッドを理解しよう】 復習
+	// 宣言
+	public String name;
+	public int age;
+
+	// コンストラクタ（初期値）
+	public Human02() {
+		name = "つげ";
+		age = 78;
+	}
+
+	// コンストラクタ（代入）
+	public Human02(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
 }
