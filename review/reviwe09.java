3fd127154963a409e5c3c01e998a3c22a7eb89c8
uedayusuke <uedayusuke1107@gmail.com>
2020-08-24 21:08:00 +0900

    [ADD]9章パッケージの作成

--- review/reviwe09.java
+++ review/reviwe09.java
@@ -1,4 +1,5 @@
 package review;
+import review.human.Human02;
 import review.human.human01;
 
 public class reviwe09 {
@@ -14,5 +15,9 @@ public class reviwe09 {
 		human01 zone = new human01("ゾーン", 39);
 		// 引数が代入されたフィールドを出力
 		System.out.println("名前は" + zone.name + "で、年齢は" + zone.age + "です");
+
+	// パッケージ
+		Human02 tsuge = new Human02();
+		System.out.println("名前は" + tsuge.name + "で、年齢は" + tsuge.age + "です。");
 	}
 }
