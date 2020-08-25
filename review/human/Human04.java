ce2715cf5e3a143b726d810dd0871d8c91a80f92
uedayusuke <uedayusuke1107@gmail.com>
2020-08-25 18:48:37 +0900

    [ADD]9章修飾子

--- review/human/Human04.java
+++ review/human/Human04.java
@@ -4,4 +4,25 @@ public class Human04 {
 	// 9章【クラスとメソッドを理解しよう】 復習
 	// 宣言
 
+	// static変数
+	static public int humanCount = 0;
+	public String name;
+
+	public static final String GREETING = "こんにちは！";
+
+	public Human04(String name) {
+		this.name = name;
+		// インスタンスが作られる毎にインクリメントする
+		Human04.humanCount++;
+	}
+
+	static public void staticMethodPrint() {
+		System.out.println("人の数は" + Human04.humanCount);
+	}
+
+	public void instanceMethodPrint() {
+		System.out.println("名前は" + name);
+		System.out.println("人の数は" + Human04.humanCount);
+	}
+
 }
