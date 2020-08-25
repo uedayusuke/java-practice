dbdfe9e27664d8c3a2cab874271f9d58eb25d43d
uedayusuke <uedayusuke1107@gmail.com>
2020-08-25 17:50:51 +0900

    [ADD]9章メソッドの追ks

--- review/human/Human03.java
+++ review/human/Human03.java
@@ -14,4 +14,18 @@ public class Human03 {
 		this.profession = "manga artist";
 	}
 
+	// 宣言
+	public String getProfile() {
+		return "年齢は" + this.age + "、職業は" + this.profession;
+	}
+
+	// 宣言
+	public void greet(String friend) {
+		if (friend == null) {
+			System.out.println("挨拶する友達がわかりません！");
+			return;
+		}
+		System.out.println(friend + "さん、こんにちは！");
+	}
+
 }
