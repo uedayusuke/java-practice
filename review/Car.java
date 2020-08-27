98d31112e176ba2991be4570a776b194fd68d957
uedayusuke <uedayusuke1107@gmail.com>
2020-08-27 20:56:00 +0900

    [ADD]9章コンストラクタの追加

--- review/Car.java
+++ review/Car.java
@@ -3,4 +3,20 @@ package review;
 public class Car {
 	// 9章【クラスとメソッドを理解しよう】 復習
 	// 確認問題
+
+	//宣言
+	// 燃料（Km/l）
+	private double fuelCost;
+	// 残量（L）
+	private double fuelAmount;
+
+	// コンストラクタ（代入）
+	public Car(double fuelCost, double fuelAmount) {
+		this.fuelCost = fuelCost;
+		this.fuelAmount = fuelAmount;
+	}
+
+	public void move(int km) {
+		System.out.println(km + "km走ります");
+	}
 }
