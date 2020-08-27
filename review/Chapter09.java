3dc6d70bcba0d9e86f1d8440dc3df69ca184132f
uedayusuke <uedayusuke1107@gmail.com>
2020-08-27 20:44:21 +0900

    [ADD]9章問題

--- review/Chapter09.java
+++ review/Chapter09.java
@@ -3,4 +3,15 @@ package review;
 public class Chapter09 {
 	// 9章【クラスとメソッドを理解しよう】 復習
 	// 確認問題
+	public static void main(String[] args) {
+		// 燃料（Km/L）
+		double fuelCost = 12.5;
+		// 残量（L）
+		double fuelAmount = 55.0;
+
+		Car car = new Car(fuelCost, fuelAmount);
+
+		car.move(20);
+		System.out.println("残量は、" + car.getFuelAmount() + "です");
+	}
 }
