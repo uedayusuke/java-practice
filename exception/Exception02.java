9df5f164fbe798d41636b7cd96f3760ad7d78355
uedayusuke <uedayusuke1107@gmail.com>
2020-09-05 16:14:06 +0900

    [ADD]10章try-catch文

--- exception/Exception02.java
+++ exception/Exception02.java
@@ -5,11 +5,16 @@ public class Exception02 {
 	// try-catch文
 	public static void main(String[] args) {
 		try {
+			// tryブロック：例外が発生しそうな処理を記述
 			System.out.println("100 ÷ 0 は？");
+			// 0で割ることはできないので例外が発生
 			int result = 100 / 0;
 			System.out.println("計算結果" + result);
+		// ArithmeticExceptionの例外が出た時の処理を記述
 		} catch (ArithmeticException e) {
+			// 例外が一致したので実行
 			System.out.println("例外が発生");
+		// finallyは例外の有無に関わらず実行される
 		} finally {
 			System.out.println("プログラム終了");
 		};
