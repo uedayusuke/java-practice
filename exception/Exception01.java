e7304b5f252e2783bb70443bdda5744a1b68215b
uedayusuke <uedayusuke1107@gmail.com>
2020-09-05 16:01:48 +0900

    [ADD]10章例外の追加

--- exception/Exception01.java
+++ exception/Exception01.java
@@ -3,6 +3,11 @@ package exception;
 public class Exception01 {
 	// 10章【例外の基本を学ぼう】復習
 	public static void main(String[] args) {
-
+		System.out.println("100 ÷ 0 は？");
+		// 0で割ったため例外が発生
+		int result = 100 / 0;
+		// 例外が発生したため以下は実行されない
+		System.out.println("計算結果" + result);
+		System.out.println("プログラム終了");
 	}
 }
