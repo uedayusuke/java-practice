5e1f040f99ba82f62bfbb8d2c4b3b79cebd1b9b9
uedayusuke <uedayusuke1107@gmail.com>
2020-09-05 16:35:07 +0900

    [ADD]10章throw文の追加

--- exception/Exception03.java
+++ exception/Exception03.java
@@ -6,10 +6,15 @@ public class Exception03 {
 	public static void main(String[] args) {
 		try {
 			Exception03.division(100, 0);
+		// ArithmeticExceptionが発生した時の例外処理
 		} catch (ArithmeticException e) {
+			// 一致しないので処理されない
 			System.out.println("ArithmeticException例外が発生");
+		// IllegalArgumentExceptionが発生した時の例外処理
 		} catch (IllegalArgumentException e) {
+			// 一致したので処理される
 			System.out.println("IllegalArgumentException例外が発生");
+			// "throw 例外インスタンス"で例外インスタンスをスロー
 			throw e;
 		}
 		System.out.println("プログラム終了");
@@ -18,6 +23,7 @@ public class Exception03 {
 	public static void division(int a, int b) {
 		System.out.println(a + "÷" + b + "は？");
 		if (b == 0) {
+			// "throw new 例外クラス"で例外インスタンスを作ってスロー
 			throw new IllegalArgumentException("引数が不正です");
 		}
 		int result = a / b;
