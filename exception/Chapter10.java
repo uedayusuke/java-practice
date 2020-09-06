ec1aa0d26cca45d8ea7ade54c43b78cfdeee78c2
uedayusuke <uedayusuke1107@gmail.com>
2020-09-06 17:28:00 +0900

    [ADD]10章メソッドの追加

--- exception/Chapter10.java
+++ exception/Chapter10.java
@@ -7,8 +7,8 @@ public class Chapter10 {
 	public static void main(String[] args) {
 		try {
 			int array[] = {1, 3, 5};
-			chapter10.varidIndex(array, 2);
-			chapter10.varidIndex(array, 3);
+			Chapter10.validIndex(array, 2);
+			Chapter10.validIndex(array, 3);
 
 			System.out.println("validIndexの呼び出し終了");
 		} catch (IllegalArgumentException e) {
@@ -27,6 +27,17 @@ public class Chapter10 {
 	// validIndexメソッドを作成
     // ・indexがarrayのサイズの範囲内なら、インデックスの要素を出力
     // ・サイズの範囲外なら、IllegalArgumentExceptionをスロー
+
+	public static void validIndex(int[] array, int index) {
+		// ifで要素が超えている場合を分岐
+		// 配列のindexは0から始まるため今回の場合0,1,2となるため3を超えると範囲外
+		if (array.length <= index) {
+			// 例外分を出力
+			throw new IllegalArgumentException(index + "はサイズの範囲外です");
+		}
+		// array[index]でindexの数字を持ってくる→indexは"0"から始まる
+		System.out.println("インデックス" + index + "の要素は" + array[index] + "です");
+	}
 
 	public static void throwSQLException() throws SQLException {
 		throw new SQLException("SQLエラーです");
