bf7230ffd5fb0a4f66d92a104ba18f71d0f256f1
uedayusuke <uedayusuke1107@gmail.com>
2020-09-06 17:41:17 +0900

    [FINISH]10章確認問題

--- exception/Chapter10.java
+++ exception/Chapter10.java
@@ -20,6 +20,14 @@ public class Chapter10 {
         // ・throwSQLExceptionメソッドを呼び出し
         // ・SQLExceptionをキャッチして、メッセージとスタックトレースを出力
         // ・"throwSQLExceptionの呼び出し終了"のメッセージを出力
+		try {
+			Chapter10.throwSQLException();
+		} catch (SQLException e) {
+			System.out.println("SQLExceptionが発生しました");
+			e.printStackTrace();
+		} finally {
+			System.out.println("throwSQLException呼び出し終了");
+		}
 
 		System.out.println("mainメソッド終了");
 	}
