4aa66f116e8af2ac66c810a27f4013a3feb46863
uedayusuke <uedayusuke1107@gmail.com>
2020-09-09 17:09:48 +0900

    [ADD]12章コンストラクタ

--- capsule/human/HumanNoCapsule.java
+++ capsule/human/HumanNoCapsule.java
@@ -2,4 +2,11 @@ package capsule.human;
 
 public class HumanNoCapsule {
 	// 12章【カプセル化を理解しよう】
+	public String name;
+	public int age;
+
+	public HumanNoCapsule(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
 }
