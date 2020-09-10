2dde503a6c2849bfc99190ec176e26b6649f66e4
uedayusuke <uedayusuke1107@gmail.com>
2020-09-10 16:38:12 +0900

    [ADD]12章getter/setter

--- capsule/human/HumanCapsule.java
+++ capsule/human/HumanCapsule.java
@@ -2,6 +2,10 @@ package capsule.human;
 
 public class HumanCapsule {
 	// 12章【カプセル化を理解しよう】
+
+	// privateをアクセス修飾子に設定し外部から隠蔽
+	// →変更を禁止できるが値の取得もできなくなる
+	// →"getter"、"setter"を作成し他のプログラムから参照できるようにする
 	private String name;
 	private int age;
 
