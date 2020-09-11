78dc459b87f30f570920863099c903b6ce41d248
uedayusuke <uedayusuke1107@gmail.com>
2020-09-11 15:45:47 +0900

    [FIX]12章クラス名を修正

--- practice12/Chapter12.java
+++ practice12/Chapter12.java
@@ -3,6 +3,5 @@ package practice12;
 public class Chapter12 {
 	// 12章 確認問題
 	public static void main(String[] args) {
-
 	}
 }
--- practice12/NoCapsuleMan.java
+++ practice12/NoCapsuleMan.java
@@ -1,11 +1,11 @@
 package practice12;
 
-public class NoCapusleMan {
+public class NoCapsuleMan {
 	// 12章 確認問題
 	public String name;
 	public int age;
 
-	public NoCapusleMan(String name, int age) {
+	public NoCapsuleMan(String name, int age) {
 		this.name = name;
 		this.age = age;
 	}
