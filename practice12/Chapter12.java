02f1285296bf05e047cdcd4887e4ea984a94bd29
uedayusuke <uedayusuke1107@gmail.com>
2020-09-11 15:51:20 +0900

    [ADD]出力を追加

--- practice12/Chapter12.java
+++ practice12/Chapter12.java
@@ -3,5 +3,10 @@ package practice12;
 public class Chapter12 {
 	// 12章 確認問題
 	public static void main(String[] args) {
+		NoCapsuleMan man = new NoCapsuleMan("小林", 32);
+		System.out.println("名前は" + man.name + "です。");
+		System.out.println("年齢は" + man.age + "です。");
+		man.age = 33;
+		System.out.println("誕生日を迎えたので、年齢が" + man.age + "になりました。");
 	}
 }
