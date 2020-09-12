4907330301138ec8ebbf7965747c07b25ae19d79
uedayusuke <uedayusuke1107@gmail.com>
2020-09-12 19:39:11 +0900

    [ADD]Humanクラス

--- extend/human/Human.java
+++ extend/human/Human.java
@@ -2,5 +2,22 @@ package extend.human;
 
 public class Human {
 	// 13章【継承を理解しよう】
+	private String name;
+	protected int age;
 
+	// コンストラクタ
+	public Human(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	// nameのgetterメソッド
+	public String getName() {
+		return this.name;
+	}
+
+	// ageのgetterメソッド
+	public int getAge() {
+		return this.age;
+	}
 }
