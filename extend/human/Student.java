cc75150595eee530fd79648e9ee45f3635ac4cfb
uedayusuke <uedayusuke1107@gmail.com>
2020-09-12 19:44:12 +0900

    [ADD]Studentクラス

--- extend/human/Student.java
+++ extend/human/Student.java
@@ -1,5 +1,19 @@
 package extend.human;
 
-public class Student {
+public class Student extends Human {
 	// 13章【継承を理解しよう】
+	private int score;
+
+	// コンストラクタ
+	public Student(String name, int age, int score) {
+		super(name, age);
+		this.score = score;
+	}
+
+	// メソッド
+	public String getStudentProfile() {
+		String profile = "年齢は" + super.age + "です。";
+		profile += "学生で、テストの点数は" + this.score + "点です。";
+		return profile;
+	}
 }
