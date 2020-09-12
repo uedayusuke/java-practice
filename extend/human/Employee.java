e35314883b4a511b6fa9e87c9500449256b59ea1
uedayusuke <uedayusuke1107@gmail.com>
2020-09-12 19:51:41 +0900

    [ADD]Employeeクラス

--- extend/human/Employee.java
+++ extend/human/Employee.java
@@ -1,5 +1,19 @@
 package extend.human;
 
-public class Employee {
+public class Employee extends Human {
 	// 13章【継承を理解しよう】
+	private String department;
+
+	// コンストラクタ
+	public Employee(String name, int age, String department) {
+		super(name, age);
+		this.department = department;
+	}
+
+	// メソッド
+	public String getEmployeeProfile() {
+		String profile = "年齢は" + super.age + "です。";
+		profile += "サラリーマンで、部署は" + this.department + "です。";
+		return profile;
+	}
 }
