0343e83c54bb75259eb36f8787ab5990034b90f3
uedayusuke <uedayusuke1107@gmail.com>
2020-09-12 20:12:15 +0900

    [ADD]出力を追加

--- extend/Extend01.java
+++ extend/Extend01.java
@@ -1,5 +1,35 @@
 package extend;
 
+import extend.human.Employee;
+import extend.human.Human;
+import extend.human.Student;
+
 public class Extend01 {
 	// 13章【継承を理解しよう】
+	public static void main(String[] args) {
+		// Humanクラスのインスタンスを生成
+		Human sugimoto = new Human("杉本", 16);
+		System.out.println("Humanクラスのメソッド:名前は、" + sugimoto.getName());
+
+		// Sutudentクラスのインスタンスを生成
+		Student sato = new Student("佐藤", 17, 70);
+		System.out.println("Humanクラスのメソッド:名前は、" + sato.getName());
+		System.out.println("Studentクラスのメソッド:プロフィールを紹介します。" + sato.getStudentProfile());
+
+		// Employyeクラスのインスタンスを生成し、アップキャストでHumanクラスを生成
+		Human human = new Employee("田中", 28, "システム部");
+		System.out.println("Humanクラスのメソッド:名前は" + human.getName());
+
+		Employee tanaka = (Employee) human;
+		System.out.println("Humanクラスのメソッド:プロフィールを紹介します。" + tanaka.getEmployeeProfile());
+
+		// printNmaeメソッドを呼び出し
+		Extend01.printName(sugimoto);
+		Extend01.printName(sato);
+		Extend01.printName(tanaka);
+	}
+
+	public static void printName(Human human) {
+		System.out.println("Humanクラスのメソッド:名前は、" + human.getName());
+	}
 }
