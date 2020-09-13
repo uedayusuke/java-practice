44055f6008be185d21eefe8f9ee656577b006cca
uedayusuke <uedayusuke1107@gmail.com>
2020-09-13 17:10:54 +0900

    [ADD]13章抽象クラス

--- extend/animal/Animal.java
+++ extend/animal/Animal.java
@@ -1,7 +1,15 @@
 package extend.animal;
+//13章【継承を理解しよう】
 
+/*
+抽象クラスの基本構文
+↓クラスの宣言
+アクセス修飾子 abstract class クラス名 {
+	↓抽象メソッド(宣言のみ!!)
+	アクセス修飾子 abstract 戻り値の型 メソッド名(引数);
+}
+ */
 public abstract class Animal {
-	// 13章【継承を理解しよう】
 	protected String name;
 
 	public void eat() {
