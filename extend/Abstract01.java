53d85c0754fb91bfd1c8a4ba9b7e5d314fd9b8c1
uedayusuke <uedayusuke1107@gmail.com>
2020-09-13 17:17:57 +0900

    [ADD]13章抽象クラス

--- extend/Abstract01.java
+++ extend/Abstract01.java
@@ -6,8 +6,12 @@ public class Abstract01 {
 	// 13章【継承を理解しよう】
 	public static void main(String[] args) {
 		Dog dog = new Dog();
+		// サブクラスを通して、Animalクラス(抽象)を呼び出し
 		dog.eat();
 		dog.cry();
+
+		// 抽象クラスはインスタンスを生成できないので要注意!!
+
 	}
 
 }
