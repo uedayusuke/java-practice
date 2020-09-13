e4f57b437bfa3ff4907b61d976eaf378589e2567
uedayusuke <uedayusuke1107@gmail.com>
2020-09-13 15:56:02 +0900

    [ADD]13章コメント

--- extend/human/Employee.java
+++ extend/human/Employee.java
@@ -1,11 +1,19 @@
 package extend.human;
 
+/*
+継承の基本構文
+修飾子 class クラス名 extends スーパークラス名 {
+	サブクラスの処理
+}
+→継承できるクラスは1つ/final修飾子は継承できない
+ */
 public class Employee extends Human {
 	// 13章【継承を理解しよう】
 	private String department;
 
 	// コンストラクタ
 	public Employee(String name, int age, String department) {
+		// 最初にスーパークラスのコンストラクタを呼び出す
 		super(name, age);
 		this.department = department;
 	}
--- extend/human/Student.java
+++ extend/human/Student.java
@@ -1,11 +1,19 @@
 package extend.human;
 
+/*
+継承の基本構文
+修飾子 class クラス名 extends スーパークラス名 {
+	サブクラスの処理
+}
+→継承できるクラスは1つ/final修飾子は継承できない
+ */
 public class Student extends Human {
 	// 13章【継承を理解しよう】
 	private int score;
 
 	// コンストラクタ
 	public Student(String name, int age, int score) {
+		// 最初にスーパークラスのコンストラクタを呼び出す
 		super(name, age);
 		this.score = score;
 	}
