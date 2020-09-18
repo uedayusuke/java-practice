c880a34794e0944b1e35dda8b1e512485e913ca6
uedayusuke <uedayusuke1107@gmail.com>
2020-09-18 16:13:55 +0900

    [ADD]14章Emoloyeeクラス

--- polymorphism/human/Employee.java
+++ polymorphism/human/Employee.java
@@ -1,6 +1,21 @@
 package polymorphism.human;
 
 //14章【ポリモーフィズムを理解しよう】
-public class Employee {
 
+// Humanクラスを継承
+public class Employee extends Human {
+	private String department;
+
+	public Employee(String name, int age, String department) {
+		// スーパークラス(Humanクラス)を呼び出す
+		super(name, age);
+		this.department = department;
+	}
+
+	// オーバーライドし、Employeeクラスに変更
+	public String getProfile() {
+		String profile = "年齢は" + this.age + "です。";
+		profile += "サラリーマンで、部署は" + this.department + "です。";
+		return profile;
+	}
 }
