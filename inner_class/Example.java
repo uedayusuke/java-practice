2c3d4f15da73e0ccba5a6715ecc3017be4f41e60
uedayusuke <uedayusuke1107@gmail.com>
2020-10-22 18:28:04 +0900

    [ADD]1章【内部クラスについて学ぼう】

--- inner_class/Example.java
+++ inner_class/Example.java
@@ -3,7 +3,9 @@ package inner_class;
 //1章【内部クラスについて学ぼう】
 public class Example {
 	public static void main(String[] args) {
+		// クラスOuterをインスタンス化
 		Outer outer = new Outer();
+		// クラスOuterの内部クラスInnerをインスタンス化
 		Outer.Inner inner = outer.new Inner();
 	}
 }
